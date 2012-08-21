acb6e01569a187e96363a49db2b01bdcaccf1f8c
Patrick Beer <patrick@pabera.com>
2012-08-21 14:02:05 +0200

    pass src and dst as parameters too

--- bin/intl.rb
+++ bin/intl.rb
@@ -75,8 +75,13 @@ class Internationalization
     save_file(pot_syntax, path[1], path[0])
   end
 
-  private
+  def set_config(root, type, value)
+    puts root, type, value
+    @config['paths'][root][type] = value
+    puts @config['paths'][root][type]
+  end
 
+  private
 
   # load config file
   def load_config
@@ -110,10 +115,28 @@ end
 # Decide wether you want to parse templates (read) or create json (write)
 intl = Internationalization.new
 
+puts ARGV.to_s
+
 if ARGV[0] == "read" || !ARGV[0]
   puts 'Read templates and create POT file'
+
+  # Use with caution: you can pass your src and dst as parameters too
+  # Currently no exception handling though
+  if ARGV[1] && ARGV[2]
+    intl.set_config('to_pot', 'src', ARGV[1])
+    intl.set_config('to_pot', 'dst', ARGV[2])
+  end
+  
   intl.create_pot
 elsif ARGV[0] == "write"
   puts 'Transform all PO to JSON files'
+  
+  # Use with caution: you can pass your src and dst as parameters too
+  # Currently no exception handling though
+  if ARGV[1] && ARGV[2]
+    intl.set_config('to_json', 'src', ARGV[1])
+    intl.set_config('to_json', 'dst', ARGV[2])
+  end
+  
   intl.po_to_json
 end
